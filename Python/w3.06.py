d5986de7bb6b7304fb8645f73424e102cb330534
w68183 <artemusenko000@gmail.com>
2022-11-22 14:00:22 +0100

    1

---
+++ Python/w3.06.py
@@ -0,0 +1,15 @@
+for i in range(1, 101):
+    print(i, end=' ')
+print()
+
+for a in range(101, -1, -1):
+    print(a, end=' ')
+print()
+
+for b in range(7, 78):
+    print(b, end=' ')
+print()
+
+for c in range(20, -1, -1):
+    print(c, end=' ')
+print()
