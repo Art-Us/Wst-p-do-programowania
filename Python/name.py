610b4c3538cb1fd52381331fd9a4768dfff56623
W68183 <artemusenko000@gmail.com>
2022-12-06 14:45:19 +0100

    1

---
+++ Python/name.py
@@ -0,0 +1,10 @@
+num1 = int(input("Wpisz liczbe 1"))
+num2 = int(input("Wpisz liczbe 2"))
+a = num1-num2
+if a > 0:
+    print("Różnica tych liczb jest dodatnia")
+else: print('Różnica tych liczb nie jest dodatnia')
+if a % 2 ==0:
+    print("Podzielna przez 2")
+else:
+    print("Nie podzielna przez 2")
---
+++ Python/w4.03.py
@@ -0,0 +1,9 @@
+zwierzeta = []
+for x in range(7):
+    z = input("Wpisz zwierząt: ")
+    zwierzeta.append(z)
+print(zwierzeta)
+zwierzeta.sort()
+print(zwierzeta)
+print(zwierzeta[0], zwierzeta[-3:],len(zwierzeta))
+
